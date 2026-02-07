a8756bb335f552510f113d774d33804c14e687a0
alokishere <alokvishwakarmabdh@gmail.com>
2026-02-07 13:17:24 +0530

    smoth scroll added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-
-import Mainroutes from './routes/Mainroutes'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Mainroutes from "./routes/Mainroutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Lenis from "lenis";
 const App = () => {
+  const lenis = new Lenis({
+    autoRaf: true,
+  });
+
   return (
     <div>
-      <Mainroutes/>
-      <ToastContainer/>
+      <Mainroutes />
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
